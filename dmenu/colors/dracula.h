b891e818158b5368a176e4f7e5b2ffd80f31153b
ocfox <i@ocfox.me>
2022-02-04 21:15:39 +0800

    rm/dmenu: local -> fetch from github

--- dmenu/colors/dracula.h
+++
@@ -1,9 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#000000", "#ff79c6" },
-	[SchemeSelHighlight] = { "#50fa7b", "#282a36" },
-	[SchemeNormHighlight] = { "#507a7b", "#282a36" },
-	[SchemeOut] = { "#000000", "#8be9fd" },
-	[SchemeMid] = { "#d7d7d7", "#1c1f24" },
-};
--- dmenu/colors/solarized-light.h
+++
@@ -1,9 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#657b83", "#fdf6e3" },
-	[SchemeSel] = { "#ffffff", "#d33682" },
-	[SchemeSelHighlight] = { "#2aa198", "#000000" },
-	[SchemeNormHighlight] = { "#2aa198", "#000000" },
-	[SchemeOut] = { "#ffffff", "#268bd2" },
-	[SchemeMid] = { "#073642", "#eee8d5" },
-};
