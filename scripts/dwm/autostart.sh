695956434f8c1e0450a323792fd3e4b2bb539e2f
ocfox <i@ocfox.me>
2022-02-04 21:19:16 +0800

    feat/script: scripts for dwm-bar

---
+++ scripts/dwm/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Uncomment this line to use those feature
+picom -b
+feh --bg-fill ~/pictures/cirno-a.png
+# feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
+$HOME/.local/share/dwm/dwm-bar &
