46056dd5238ba49409fd58b6bb1a42c5bf7727dd
ocfox <i@ocfox.me>
2022-02-06 21:16:21 +0800

    feat/conky: new decoration conky

--- scripts/dwm/autostart.sh
+++ scripts/dwm/autostart.sh
@@ -2,6 +2,8 @@
 
 # Uncomment this line to use those feature
 picom -b
+conky -q
 feh --bg-fill ~/pictures/cirno-a.png
+
 # feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
 $HOME/.local/share/dwm/dwm-bar &
