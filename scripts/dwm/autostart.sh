2a43a050bf12d0e78a9a305d89a8b3156b7647f2
ocfox <i@ocfox.me>
2022-02-12 20:05:31 +0800

    scripts: fix autostart wallpaper

--- scripts/dwm/autostart.sh
+++ scripts/dwm/autostart.sh
@@ -2,7 +2,6 @@
 
 # Uncomment this line to use those feature
 picom -b
-feh --bg-fill ~/pictures/cirno-a.png
 
 # feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
 $HOME/.local/share/dwm/dwm-bar &
