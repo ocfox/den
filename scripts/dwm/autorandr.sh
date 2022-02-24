a0b6d8702ead9ebdd45a60833cf0e1f587b12e39
ocfox <i@ocfox.me>
2022-02-24 16:23:20 +0800

    conky: add a big time display

---
+++ scripts/dwm/autorandr.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate right --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-1 --off
--- scripts/dwm/autostart.sh
+++ scripts/dwm/autostart.sh
@@ -5,3 +5,6 @@ picom -b
 
 # feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
 $HOME/.local/share/dwm/dwm-bar &
+
+$HOME/.local/share/dwm/autorandr.sh
+feh --bg-fill Pictures/Wallpapers/cirno-a.png Pictures/Wallpapers/cirno-e.png
