ad2f243392a4e648c1ec079c85f20f942c314dac
ocfox <i@ocfox.me>
2022-03-04 21:04:15 +0800

    feat/flake: add mozilla overlay support

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
   description = "nixos-config";
 
+  inputs.nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-#inputs.dwm.url = "github:ocfox/dwm";
+  # inputs.dwm.url = "github:ocfox/dwm";
   inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   inputs.nur.url = github:nix-community/NUR;
-  # inputs.home-manager = {
-  #   url = "github:nix-community/home-manager";
-  #   inputs.nixpkgs.follows = "nixpkgs";
-  # };
+  inputs.home-manager = {
+    url = "github:nix-community/home-manager";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, nur, neovim-nightly-overlay, home-manager, ...}@inputs:
+  outputs = { self, nixpkgs-mozilla, nixpkgs, nur, neovim-nightly-overlay, home-manager, ...}@inputs:
     let
       system = "x86_64-linux";
       # overlays = [
@@ -18,24 +19,28 @@
       #   ];
     in {
 
-      # homeConfigurations.ocfox = home-manager.lib.homeManagerConfiguration {
-      #   system = null;
-      #   pkgs = self.legacyPackages."x86_64-linux";
-      #   stateVersion = "unstable";
-      #
-      #   homeDirectory = "/home/ocfox";
-      #   username = "ocfox";
-      #   configuration = { pkgs, ... }:
-      #       {
-      #         nixpkgs.overlays = overlays;
-      #       };
-      # };
+      homeConfigurations.ocfox = home-manager.lib.homeManagerConfiguration {
+        pkgs = self.legacyPackages."x86_64-linux";
+        stateVersion = "unstable";
+
+        homeDirectory = "/home/ocfox";
+        username = "ocfox";
+        configuration = { pkgs, ... }:
+            {
+              xsession.enable = true;
+              xsession.pointerCursor = {
+                package = pkgs.gnome.adwaita-icon-theme;
+                name = "Adwaita";
+                size = 38;
+              };
+            };
+      };
 
       nixosConfigurations.whitefox =  nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           ./configuration.nix
-          { nixpkgs.overlays = [ nur.overlay neovim-nightly-overlay.overlay ]; }
+          { nixpkgs.overlays = [ nur.overlay neovim-nightly-overlay.overlay nixpkgs-mozilla.overlay ]; }
         ];
         specialArgs = { inherit inputs; };
       };
