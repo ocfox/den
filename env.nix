12dbaafdcd679948b599ab8c16829c419cb084d9
ocfox <i@ocfox.me>
2022-05-12 22:53:31 +0800

    config/env: enable input method for kitty

--- env.nix
+++ env.nix
@@ -1,9 +1,14 @@
-{ config, pkgs, ... }:
 {
-
+  config,
+  pkgs,
+  ...
+}: {
   environment.sessionVariables = rec {
     CARGO_HOME = "\${HOME}/.cargo";
     CARGO_BIN = "\${HOME}/.cargo/bin";
+
+    # enable fcitx5/ibus input method for kitty
+    GLFW_IM_MODULE = "ibus";
 
     PATH = [
       "\${CARGO_BIN}"
