7425df26424b62552854967fe6f40d7eaaa6b424
ocfox <i@ocfox.me>
2022-07-14 12:36:48 +0800

    script: fcitx5 would not startup with X now

--- scripts/dwm/autostart.sh
+++ scripts/dwm/autostart.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Uncomment this line to use those feature
-picom -b
+# picom -b
 
 # feh --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
-$HOME/.local/share/dwm/dwm-bar &
 
 $HOME/.local/share/dwm/autorandr.sh
-feh --bg-fill Pictures/Wallpapers/cirno-a.png Pictures/Wallpapers/cirno-e.png
+$HOME/.local/share/dwm/dwm-bar &
+
+fcitx5
