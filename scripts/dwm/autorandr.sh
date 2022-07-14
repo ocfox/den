49c5a1280fb0f45757a1d66164d4fd12fdb44ee1
ocfox <i@ocfox.me>
2022-07-14 12:35:47 +0800

    scripts/dwm: fix conky startup when only one monitor

--- scripts/dwm/autorandr.sh
+++ scripts/dwm/autorandr.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 2560x0 --rotate right --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-1 --off
+xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x360 --rotate inverted --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+
+feh --bg-fill ~/Pictures/Wallpapers/rurudo.jpg ~/Pictures/Wallpapers/rurudo-purple.jpg
