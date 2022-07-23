f5fb97c29ead7614f2bf93d35a4e9e11809e1c5b
ocfox <i@ocfox.me>
2022-07-23 23:00:42 +0800

    env: remove useless env

--- env.nix
+++ env.nix
@@ -8,19 +8,12 @@
       CARGO_HOME = "\${HOME}/.cargo";
       CARGO_BIN = "\${HOME}/.cargo/bin";
 
-      # enable fcitx5/ibus input method for kitty
       GLFW_IM_MODULE = "ibus";
 
       PATH = [
         "\${CARGO_BIN}"
         "\${CARGO_HOME}"
       ];
-    };
-
-    variables = {
-      GDK_SCALE = "2";
-      GDK_DPI_SCALE = "0.5";
-      _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
     };
   };
 }
