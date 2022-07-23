85cfec8c845173b629428906eacca20861ff2a6f
ocfox <i@ocfox.me>
2022-07-23 14:57:00 +0800

    feat: waybar config

--- programs/waybar.nix
+++ programs/waybar.nix
@@ -1,82 +1,76 @@
 { pkgs }:
 {
-    "layer": "top", // Waybar at top layer
-    "position": "top", // Waybar position (top|bottom|left|right)
-    "modules-right": [ "cpu", "temperature", "pulseaudio", "bluetooth", "network", "tray"],
-    "modules-left":[ "clock" ],
-    // Modules configuration
-    "idle_inhibitor": {
-        "format": "{icon}",
-        "format-icons": {
-            "activated": "",
-            "deactivated": ""
-        },
-        "tooltip": "true"
-    },
-    "tray": {
-        //"icon-size": 11,
-        "spacing": 5
-    },
-    "clock": {
-        "format": "  {:%H:%M   %e %b}",
-        "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>",
-        "today-format": "<b>{}</b>",
-        "on-click": "gnome-calendar"
-    },
-    "cpu": {
-        "interval": "1",
-        "format": "﬙  {max_frequency}GHz <span color=\"darkgray\">| {usage}%</span>",
-        "max-length": 13,
-        "min-length": 13,
-        "on-click": "kitty -e htop --sort-key PERCENT_CPU",
-        "tooltip": false
-    },
-    "temperature": {
-        //"thermal-zone": 1,
-        "interval": "4",
-        "hwmon-path": "/sys/class/hwmon/hwmon3/temp1_input",
-        "critical-threshold": 74,
-        "format-critical": "  {temperatureC}°C",
-        "format": "{icon}  {temperatureC}°C",
-        "format-icons": ["", "", ""],
-        "max-length": 7,
-        "min-length": 7
-    },
-    "network": {
-        // "interface": "wlan0", // (Optional) To force the use of this interface,
-        "format-wifi": "  {essid}",
-        "format-ethernet": "{ifname}: {ipaddr}/{cidr} ",
-        "format-linked": "{ifname} (No IP) ",
-        "format-disconnected": "",
-        "format-alt": "{ifname}: {ipaddr}/{cidr}",
-        "family": "ipv4",
-        "tooltip-format-wifi": "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\n {bandwidthUpBits}  {bandwidthDownBits}",
-        "tooltip-format-ethernet": " {ifname}\nIP: {ipaddr}\n {bandwidthUpBits}  {bandwidthDownBits}"
-    },
-    "pulseaudio": {
-        "scroll-step": 3, // %, can be a float
-        "format": "{icon} {volume}% {format_source}",
-        "format-bluetooth": "{volume}% {icon} {format_source}",
-        "format-bluetooth-muted": " {icon} {format_source}",
-        "format-muted": " {format_source}",
-        //"format-source": "{volume}% ",
-        //"format-source-muted": "",
-        "format-source": "",
-        "format-source-muted": "",
-        "format-icons": {
-            "headphone": "",
-            "hands-free": "",
-            "headset": "",
-            "phone": "",
-            "portable": "",
-            "car": "",
-            "default": ["", "", ""]
-        },
-        "on-click": "pavucontrol",
-        "on-click-right": "pactl set-source-mute @DEFAULT_SOURCE@ toggle"
-    },
-    "custom/weather": {
-        "exec": "curl 'https://wttr.in/?format=1'",
-        "interval": 3600
-    },
+    "layer" = "top";
+    "position" = "top";
+    "modules-right" = [ "cpu" "temperature" "pulseaudio" "bluetooth" "network" "tray"];
+    "modules-left" = [ "clock" ];
+    "idle_inhibitor" = {
+        "format" = "{icon}";
+        "format-icons" = {
+            "activated" = "";
+            "deactivated" = "";
+        };
+        "tooltip" = "true";
+    };
+    "tray" = {
+        "spacing" = 5;
+    };
+    "clock" = {
+        "format" = "  {:%H:%M   %e %b}";
+        "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        "today-format" = "<b>{}</b>";
+        "on-click" = "gnome-calendar";
+    };
+    "cpu" = {
+        "interval" = "1";
+        "format" = "﬙  {max_frequency}GHz <span color=\"darkgray\">| {usage}%</span>";
+        "max-length" = 13;
+        "min-length" = 13;
+        "on-click" = "kitty -e htop --sort-key PERCENT_CPU";
+        "tooltip" = false;
+    };
+    "temperature" = {
+        "interval" = "4";
+        "hwmon-path" = "/sys/class/hwmon/hwmon3/temp1_input";
+        "critical-threshold" = 74;
+        "format-critical" = "  {temperatureC}°C";
+        "format" = "{icon}  {temperatureC}°C";
+        "format-icons" = ["" "" ""];
+        "max-length" = 7;
+        "min-length" = 7;
+    };
+    "network" = {
+        "format-wifi" = "  {essid}";
+        "format-ethernet" = "{ifname}: {ipaddr}/{cidr} ";
+        "format-linked" = "{ifname} (No IP) ";
+        "format-disconnected" = "";
+        "format-alt" = "{ifname}: {ipaddr}/{cidr}";
+        "family" = "ipv4";
+        "tooltip-format-wifi" = "  {ifname} @ {essid}\nIP: {ipaddr}\nStrength: {signalStrength}%\nFreq: {frequency}MHz\n {bandwidthUpBits}  {bandwidthDownBits}";
+        "tooltip-format-ethernet" = " {ifname}\nIP: {ipaddr}\n {bandwidthUpBits}  {bandwidthDownBits}";
+    };
+    "pulseaudio" = {
+        "scroll-step" = 3;
+        "format" = "{icon} {volume}% {format_source}";
+        "format-bluetooth" = "{volume}% {icon} {format_source}";
+        "format-bluetooth-muted" = " {icon} {format_source}";
+        "format-muted" = " {format_source}";
+        "format-source" = "";
+        "format-source-muted" = "";
+        "format-icons" = {
+            "headphone" = "";
+            "hands-free" = "";
+            "headset" = "";
+            "phone" = "";
+            "portable" = "";
+            "car" = "";
+            "default" = ["" "" ""];
+        };
+        "on-click" = "pavucontrol";
+        "on-click-right" = "pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+    };
+    "custom/weather" = {
+        "exec" = "curl 'https://wttr.in/?format=1'";
+        "interval" = 3600;
+    };
 }
