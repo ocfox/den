06b3c31b3957aa18fee625328e98f7194b52cd48
ocfox <i@ocfox.me>
2022-08-05 22:54:12 +0800

    remove scripts

--- scripts/dwm/autorandr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-0 --mode 1920x1080 --pos 2560x360 --rotate inverted --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal
-
-feh --bg-fill ~/Pictures/Wallpapers/rurudo.jpg ~/Pictures/Wallpapers/rurudo-purple.jpg
