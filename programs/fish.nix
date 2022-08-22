736439925257cf8aca86b6550a4f43c9ed1a0499
ocfox <i@ocfox.me>
2022-08-22 19:44:27 +0800

    fish/functions: add haskellEnv & nix-shell status

--- programs/fish.nix
+++ programs/fish.nix
@@ -11,6 +11,7 @@
       nixup = "nixos-rebuild switch --use-remote-sudo --flake $HOME/nixos";
       ls = "exa -l";
       top = "btm";
+      nixs = "nix-shell --run fish";
     };
 
     loginShellInit = ''
@@ -18,7 +19,7 @@
         exec sway
       end
     '';
-
+    
     plugins = [
       {
         name = "babelfish";
@@ -32,18 +33,29 @@
     ];
 
     functions = {
-      fish_prompt = "
-          echo -n (set_color blue)' '
-          if test -n \"$SSH_TTY\"
-              echo -n (set_color brred)\"$USER\"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
+      fish_greeting = "${pkgs.fortune}/bin/fortune";
+
+      haskellEnv = ''
+        nix-shell -p "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ $argv ])"
+      '';
+
+      fish_prompt = ''
+          if test -n "$IN_NIX_SHELL"
+            echo -n (set_color red)' '
+          else
+            echo -n (set_color blue)' '
           end
-          echo -n (set_color blue)(prompt_pwd)''
+          if test -n "$SSH_TTY"
+              echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)
+          end
+          echo -n (set_color blue)(prompt_pwd)
           set_color -o
           if fish_is_root_user
               echo -n (set_color red)'# '
           end
           echo -n (set_color green)' ~>'
-          set_color normal";
+          set_color normal
+          '';
 
       fish_right_prompt = "
           set -l cmd_status $status
