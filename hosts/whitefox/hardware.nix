9b2d16421eb4c1dbdfdd2e45b61409e47ce418ab
ocfox <i@ocfox.me>
2022-11-24 10:05:47 +0800

    mount win partition

--- hosts/whitefox/hardware.nix
+++ hosts/whitefox/hardware.nix
@@ -12,10 +12,17 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.supportedFilesystems = [ "ntfs" ];
 
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/d666a5ff-d7d0-4883-a5dc-5e7b8bd7845e";
     fsType = "ext4";
+  };
+
+  fileSystems."/run/mount/win" = {
+    device = "/dev/disk/by-uuid/B86695766695365A";
+    fsType = "ntfs3";
+    options = [ "rw" "uid=1000" ];
   };
 
   fileSystems."/boot" = {
