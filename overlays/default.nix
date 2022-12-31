2170557e49354978da126da54f42170dd2f9be71
ocfox <i@ocfox.me>
2022-12-31 17:09:48 +0800

    overlays: remove sway input patch

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,18 +10,6 @@
         username = "ocfox";
         token = "483926ff66c4429ec6084fc0533c17";
       };
-
-      sway-unwrapped = super.sway-unwrapped.overrideAttrs (old: {
-        patches =
-          (old.patches or [ ])
-          ++ [
-            (super.fetchpatch {
-              url = "https://github.com/swaywm/sway/commit/9debeb40ceaeae9e577bddcc248a36d99f0a066f.patch";
-              sha256 = "sha256-WRB6PuENMnMJV/cUrWLGoi7Aw+wuGi1GaFITJkqB69k=";
-            })
-          ];
-      });
     })
-
   ];
 }
