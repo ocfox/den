8f1ae804410919f6fee2988b0461e4ebd52fe189
ocfox <i@ocfox.me>
2023-01-10 13:56:03 +0800

    tmux: add vim plugin & enable mouse

--- programs/tmux.nix
+++ programs/tmux.nix
@@ -8,10 +8,19 @@
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
     prefix = "`";
+    baseIndex = 1;
+    escapeTime = 0;
+    customPaneNavigationAndResize = true;
     keyMode = "vi";
+
     plugins = with pkgs.tmuxPlugins; [
-      nord
+      onedark-theme
       prefix-highlight
+      vim-tmux-navigator
     ];
+
+    extraConfig = ''
+      set -g mouse on
+    '';
   };
 }
