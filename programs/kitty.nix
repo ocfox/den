128caf9a9986cd2fb157b76db2bc3b80d1922d88
ocfox <i@ocfox.me>
2023-01-25 17:43:56 +0800

    kitty: enable opacity

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -10,7 +10,7 @@
       size = 12;
     };
     settings = {
-      # background_opacity = "0.9";
+      background_opacity = "0.9";
       confirm_os_window_close = "0";
     };
 
@@ -23,7 +23,7 @@
 
     extraConfig = "
       remember_window_size  no
-      background  #2b3339
+      background  #272e33
       foreground  #d8caac
 
       cursor                #d8caac
