4ac6e34f86ff7a6cdb4341f05020e65fa5ec21df
ocfox <i@ocfox.me>
2023-04-26 09:29:07 +0800

    darwin: fix PATH order

--- home-darwin/programs/fish.nix
+++ home-darwin/programs/fish.nix
@@ -1,4 +1,6 @@
 {
+  loginShellInit = ''for p in (string split " " $NIX_PROFILES); fish_add_path --prepend --move $p/bin; end'';
+
   functions = {
     rebuild = ''
       darwin-rebuild switch --flake $HOME/nixos#silverfox
