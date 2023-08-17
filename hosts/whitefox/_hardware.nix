f03534fca01e0a5f648954a789ce30ac03f133d2
ocfox <i@ocfox.me>
2023-08-17 09:41:05 +0800

    whitefox/hardware: update

--- hosts/whitefox/_hardware.nix
+++ hosts/whitefox/_hardware.nix
@@ -1,45 +1,31 @@
 { config
 , lib
 , modulesPath
-, pkgs
 , ...
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
   boot.supportedFilesystems = [ "ntfs" ];
 
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/0ca7545c-30e4-4f9a-b3ed-dde411aa6084";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/29E1-98CC";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/baced005-ee3c-4db1-b466-2d7fabdc3970"; }];
-
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      nvidia-vaapi-driver
-    ];
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/fe0ecfb9-db21-43f0-915a-70c37765f181";
+    fsType = "btrfs";
   };
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = false;
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/3307-5F4E";
+    fsType = "vfat";
   };
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  swapDevices = [ ];
+
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.opengl.enable = true;
+  hardware.keyboard.qmk.enable = true;
 }
