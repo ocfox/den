571b77c7533d0c550a2fb136ea262f709de3cc82
ocfox <i@ocfox.me>
2024-01-23 00:09:08 +0800

    arcticfox: use networkd

--- hosts/arcticfox/networking.nix
+++ hosts/arcticfox/networking.nix
@@ -2,10 +2,6 @@
   firewall.enable = false;
   hostName = "arcticfox";
 
-  useDHCP = false;
-  
-  interfaces.eno1.useDHCP = true;
-  interfaces.eno2.useDHCP = true;
-  interfaces.eno3.useDHCP = true;
-  interfaces.eno4.useDHCP = true;
+  useDHCP = true;
+  useNetworkd = true;
 }
