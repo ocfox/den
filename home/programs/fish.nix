45cb3d9df8f341944b5bb03014541b8906d86672
ocfox <i@ocfox.me>
2024-01-24 00:46:13 +0800

    fish: add func refresh_tmux_vars

--- home/programs/fish.nix
+++ home/programs/fish.nix
@@ -21,6 +21,13 @@
 
     ns = ''nix shell nixpkgs#{ $argv }'';
 
+    # fix ssh-agent forwarding when attach to tmux
+    refresh_tmux_vars = ''
+      if set -q TMUX
+        tmux showenv -s | string replace -rf '^((?:SSH).*?)=(".*?"); export.*' 'set -gx $1 $2' | source
+      end
+    '';
+
     haskellEnv = ''
       nix-shell -p haskell-language-server "haskellPackages.ghcWithPackages (pkgs: with pkgs; [ $argv ])"
     '';
