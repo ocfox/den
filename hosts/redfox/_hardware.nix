54da750878b5d10fa7375abe85947f96383e6b0e
ocfox <i@ocfox.me>
2024-01-23 19:41:21 +0800

    fix redfox disko

--- hosts/redfox/_hardware.nix
+++ hosts/redfox/_hardware.nix
@@ -1,24 +1,6 @@
-{ lib, ... }:
-
 {
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "ehci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
   boot.initrd.kernelModules = [ "dm-snapshot" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    {
-      device = "/dev/disk/by-uuid/0c4cf42a-6b50-4553-bdf6-c4d9d2a7a759";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/1E1B-5679";
-      fsType = "vfat";
-    };
-
-  swapDevices = [ ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
