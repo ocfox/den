6e25fd66f0e9e1c9cc0f8b76b16558e2894d8882
ocfox <i@ocfox.me>
2024-01-24 14:18:49 +0800

    home: git configure

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -1,3 +1,12 @@
 {
   enable = true;
+  userName = "ocfox";
+  userEmail = "i@ocfox.me";
+  extraConfig.gpg.format = "ssh";
+
+  signing = {
+    signByDefault = true;
+    key =
+      "key::sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHnLWTS5/vPyPFY+tCVYn3Ejf3NQpQzcGnWLQTyE7lbzAAAAC3NzaDpwYXNzZm94";
+  };
 }
