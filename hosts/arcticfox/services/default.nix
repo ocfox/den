c0b0f409bcff5040b115e75179a875590b616929
ocfox <i@ocfox.me>
2024-01-24 14:18:38 +0800

    arctic: media

---
+++ hosts/arcticfox/services/default.nix
@@ -0,0 +1,23 @@
+{ pkgs }:
+{
+  openssh.enable = true;
+
+  # TODO: meadia space
+  jellyfin = {
+    enable = true;
+    openFirewall = true;
+    user = "transmission";
+  };
+
+  transmission = {
+    enable = true;
+    user = "transmission";
+    openFirewall = true;
+    openRPCPort = true;
+    webHome = pkgs.flood-for-transmission;
+    settings = {
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "127.0.0.1,192.168.31.149";
+    };
+  };
+}
