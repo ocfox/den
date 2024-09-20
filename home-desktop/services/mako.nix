5c408d0a6c49b0abe7ddecf60ad8c31ff1781a01
ocfox <i@ocfox.me>
2024-09-20 11:38:43 +0800

    mako: add border radius

--- home-desktop/services/mako.nix
+++ home-desktop/services/mako.nix
@@ -2,6 +2,7 @@
   enable = true;
   backgroundColor = "#2b3339CC";
   borderSize = 0;
+  borderRadius = 5;
   borderColor = "#83b6af00";
   defaultTimeout = 5000;
   font = "JetBrainsMono Nerd Font 12";
