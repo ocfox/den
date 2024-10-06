10f3a32b95a2086526789088435e2b267f2acdd7
ocfox <i@ocfox.me>
2024-10-06 11:56:47 +0800

    vulpes: acme

--- hosts/vulpes/services.nix
+++ hosts/vulpes/services.nix
@@ -3,4 +3,18 @@
   openssh.enable = true;
 
   tailscale.enable = true;
+
+  nginx = {
+    enable = true;
+    virtualHosts."vulpes.ocfox.me" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        return = "200 '<html><body>The Vulpes.</body></html>'";
+        extraConfig = ''
+          default_type text/html;
+        '';
+      };
+    };
+  };
 }
