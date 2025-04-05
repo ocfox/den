a95695b393b44cae74a07b45c04378f58740d238
ocfox <i@ocfox.me>
2025-04-05 20:00:17 +0800

    inputs: update

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,6 +1,6 @@
 { lib, pkgs, ... }:
 {
-  enable = false;
+  enable = true;
   font = {
     name = "JetBrainsMono Nerd Font";
   };
@@ -18,52 +18,54 @@
     "cmd+w" = "close_tab";
   };
 
-  extraConfig = ''
-    remember_window_size  no
-    background  #2b3339
-    foreground  #d8caac
+  themeFile = "everforest_dark_hard";
 
-    cursor                #d8caac
+  # extraConfig = ''
+  #   remember_window_size  no
+  #   background  #2b3339
+  #   foreground  #d8caac
 
-    selection_foreground  #d8caac
-    selection_background  #505a60
+  #   cursor                #d8caac
 
-    color0  #3c474d
-    color8  #868d80
+  #   selection_foreground  #d8caac
+  #   selection_background  #505a60
 
-    # red
-    color1                #e68183
-    # light red
-    color9                #e68183
+  #   color0  #3c474d
+  #   color8  #868d80
 
-    # green
-    color2                #a7c080
-    # light green
-    color10               #a7c080
+  #   # red
+  #   color1                #e68183
+  #   # light red
+  #   color9                #e68183
 
-    # yellow
-    color3                #d9bb80
-    # light yellow
-    color11               #d9bb80
+  #   # green
+  #   color2                #a7c080
+  #   # light green
+  #   color10               #a7c080
 
-    # blue
-    color4                #83b6af
-    # light blue
-    color12               #83b6af
+  #   # yellow
+  #   color3                #d9bb80
+  #   # light yellow
+  #   color11               #d9bb80
 
-    # magenta
-    color5                #d39bb6
-    # light magenta
-    color13               #d39bb6
+  #   # blue
+  #   color4                #83b6af
+  #   # light blue
+  #   color12               #83b6af
 
-    # cyan
-    color6                #87c095
-    # light cyan
-    color14               #87c095
+  #   # magenta
+  #   color5                #d39bb6
+  #   # light magenta
+  #   color13               #d39bb6
 
-    # light gray
-    color7                #868d80
-    # dark gray
-    color15               #868d80
-  '';
+  #   # cyan
+  #   color6                #87c095
+  #   # light cyan
+  #   color14               #87c095
+
+  #   # light gray
+  #   color7                #868d80
+  #   # dark gray
+  #   color15               #868d80
+  # '';
 }
