e43679942550c71617f8dab7f0ab2dd9bcd5da51
ocfox <i@ocfox.me>
2025-04-20 15:59:44 +0800

    animeko: 4.8.2 -> 4.9.0

--- pkgs/animeko/package.nix
+++ pkgs/animeko/package.nix
@@ -5,11 +5,11 @@
 }:
 
 let
-  version = "4.8.2";
+  version = "4.9.0";
 
   appimage = fetchurl {
     url = "https://github.com/open-ani/animeko/releases/download/v${version}/ani-${version}-linux-x86_64.appimage";
-    sha256 = "sha256-I088DNHYZEf19x9Sv0NVFrqs+ip0AhB1otDyksoqX9U=";
+    sha256 = "sha256-5WWHBnl/uFzT6W9Cx12MO0obj8PhYfffNwHH5lIMgcg=";
   };
 in
 
