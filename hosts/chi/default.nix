3c32acf9889c9d47753669060a6b603ce2d4448b
ocfox <i@ocfox.me>
2025-05-19 21:46:04 +0800

    fix networking

--- hosts/chi/default.nix
+++ hosts/chi/default.nix
@@ -37,5 +37,11 @@
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHnLWTS5/vPyPFY+tCVYn3Ejf3NQpQzcGnWLQTyE7lbzAAAAC3NzaDpwYXNzZm94"
   ];
 
+  users.users.ocfox = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialHashedPassword = "$6$WqXVstk7pHt0WTmx$5s.SJYuX1g2nR1biNPVAyrQ5pxUaAWUf96Sw0.Aca8vsMoUQg8KBP6lF4wsVO4lzs4gcmffuVQWdQMxjX3loU/";
+  };
+
   system.stateVersion = "24.11";
 }
