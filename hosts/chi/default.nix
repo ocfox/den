c5c660c508ba40d2cd3f2d64c06856d527d5758b
ocfox <i@ocfox.me>
2025-05-20 18:25:32 +0800

    fix chi

--- hosts/chi/default.nix
+++ hosts/chi/default.nix
@@ -2,7 +2,6 @@
   modulesPath,
   config,
   pkgs,
-  self,
   ...
 }:
 {
@@ -68,12 +67,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIHnLWTS5/vPyPFY+tCVYn3Ejf3NQpQzcGnWLQTyE7lbzAAAAC3NzaDpwYXNzZm94"
   ];
-
-  users.users.ocfox = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    initialHashedPassword = "$6$WqXVstk7pHt0WTmx$5s.SJYuX1g2nR1biNPVAyrQ5pxUaAWUf96Sw0.Aca8vsMoUQg8KBP6lF4wsVO4lzs4gcmffuVQWdQMxjX3loU/";
-  };
 
   system.stateVersion = "24.11";
 }
