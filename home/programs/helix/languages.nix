1d919dbffa5738ac0ebd3e7a191baee2632bade1
ocfox <i@ocfox.me>
2025-09-16 12:27:06 +0800

    helix need some more

--- home/programs/helix/languages.nix
+++ home/programs/helix/languages.nix
@@ -7,21 +7,21 @@
         command = "${lib.getExe pkgs.nixfmt-rfc-style}";
       };
     }
-    {
-      name = "typescript";
-      file-types = [
-        "ts"
-        "tsx"
-      ];
-      language-servers = [ "deno-language-server" ];
-    }
+    # {
+    #   name = "typescript";
+    #   file-types = [
+    #     "ts"
+    #     "tsx"
+    #   ];
+    #   language-servers = [ "deno-language-server" ];
+    # }
   ];
 
-  language-server.deno-language-server = {
-    command = "${lib.getExe pkgs.deno}";
-    args = [ "lsp" ];
-    required-root-patterns = [
-      "deno.json"
-    ];
-  };
+  # language-server.deno-language-server = {
+  #   command = "${lib.getExe pkgs.deno}";
+  #   args = [ "lsp" ];
+  #   required-root-patterns = [
+  #     "deno.json"
+  #   ];
+  # };
 }
