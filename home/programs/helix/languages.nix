f9bfa8f661c23db47dad9648f0c127514e23cfd9
ocfox <i@ocfox.me>
2025-10-11 03:07:20 +0800

    helix lang++

--- home/programs/helix/languages.nix
+++ home/programs/helix/languages.nix
@@ -7,21 +7,79 @@
         command = "${lib.getExe pkgs.nixfmt-rfc-style}";
       };
     }
-    # {
-    #   name = "typescript";
-    #   file-types = [
-    #     "ts"
-    #     "tsx"
-    #   ];
-    #   language-servers = [ "deno-language-server" ];
-    # }
+    {
+      name = "typescript";
+      auto-format = true;
+      language-servers = [
+        # "dprint"
+        "typescript-language-server"
+      ];
+    }
   ];
 
-  # language-server.deno-language-server = {
-  #   command = "${lib.getExe pkgs.deno}";
-  #   args = [ "lsp" ];
-  #   required-root-patterns = [
-  #     "deno.json"
-  #   ];
-  # };
+  language-server = {
+    deno-lsp = {
+      command = lib.getExe pkgs.deno;
+      args = [ "lsp" ];
+      environment.NO_COLOR = "1";
+      config.deno = {
+        enable = true;
+        lint = true;
+        unstable = true;
+        suggest = {
+          completeFunctionCalls = false;
+          imports = {
+            hosts."https://deno.land" = true;
+          };
+        };
+        inlayHints = {
+          enumMemberValues.enabled = true;
+          functionLikeReturnTypes.enabled = true;
+          parameterNames.enabled = "all";
+          parameterTypes.enabled = true;
+          propertyDeclarationTypes.enabled = true;
+          variableTypes.enabled = true;
+        };
+      };
+    };
+
+    typescript-language-server = {
+      command = lib.getExe pkgs.nodePackages.typescript-language-server;
+      args = [ "--stdio" ];
+      # config = {
+      #   typescript-language-server.source = {
+      #     addMissingImports.ts = true;
+      #     fixAll.ts = true;
+      #     organizeImports.ts = true;
+      #     removeUnusedImports.ts = true;
+      #     sortImports.ts = true;
+      #   };
+      # };
+    };
+
+    vscode-css-language-server = {
+      command = "${pkgs.nodePackages.vscode-langservers-extracted}/bin/vscode-css-language-server";
+      args = [ "--stdio" ];
+      config = {
+        provideFormatter = true;
+        css.validate.enable = true;
+        scss.validate.enable = true;
+      };
+    };
+
+    ruff = {
+      command = lib.getExe pkgs.ruff;
+      args = [ "server" ];
+    };
+
+    dprint = {
+      command = lib.getExe pkgs.dprint;
+      args = [ "lsp" ];
+    };
+
+    vscode-json-language-server = {
+      command = lib.getExe pkgs.vscode-json-languageserver;
+      args = [ "--stdio" ];
+    };
+  };
 }
