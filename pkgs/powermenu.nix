e8d272b495657e11961a005685ceb0cf1ff9b708
ocfox <i@ocfox.me>
2025-10-25 15:04:06 +0800

    *powermenu

--- pkgs/powermenu.nix
+++ pkgs/powermenu.nix
@@ -1,17 +1,15 @@
 { lib, pkgs }:
 pkgs.writeShellScriptBin "powermenu" ''
-  exec $(
-    echo -e "  Poweroff\n  Reboot\n  Suspend\n  Hibernate" | \
-    ${lib.getExe pkgs.fuzzel} -d
-  ) | \
-  case "$(cat -)" in
-    *Poweroff)
-      systemctl poweroff ;;
-    *Reboot)
-      systemctl reboot ;;
-    *Suspend)
-      systemctl suspend ;;
-    *Hibernate)
-      systemctl hibernate ;;
-  esac
+  echo -e "  Poweroff\n  Reboot\n  Suspend\n  Hibernate" | \
+    ${lib.getExe pkgs.fuzzel} -d | \
+    case "$(cat -)" in
+      *Poweroff)
+        systemctl poweroff ;;
+      *Reboot)
+        systemctl reboot ;;
+      *Suspend)
+        systemctl suspend ;;
+      *Hibernate)
+        systemctl hibernate ;;
+    esac
 ''
