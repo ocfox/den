d5828886d34d6ed1efc4774ad75ee8f38f63cbdc
ocfox <i@ocfox.me>
2025-11-26 23:31:00 +0800

    +wall

--- modules/hosts/default.nix
+++ modules/hosts/default.nix
@@ -5,6 +5,7 @@ in
 {
   flake.nixosConfigurations = mkNixosFromAttrs {
     brick = "x86_64-linux";
+    wall = "x86_64-linux";
     clare = "x86_64-linux";
     mizu = "x86_64-linux";
     cave = "aarch64-linux";
