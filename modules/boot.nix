a4a4b1daa433dcfebb8eb6b1e9baf3b9fd7abb73
ocfox <i@ocfox.me>
2025-12-07 15:35:18 +0800

    ^

--- modules/boot.nix
+++ modules/boot.nix
@@ -10,10 +10,10 @@
         efi.canTouchEfiVariables = true;
       };
 
-      system.nixos-init.enable = true;
-      system.etc.overlay.enable = true;
-      system.etc.overlay.mutable = false;
-      boot.initrd.systemd.enable = true;
+      # system.nixos-init.enable = true;
+      # system.etc.overlay.enable = true;
+      # system.etc.overlay.mutable = false;
+      # boot.initrd.systemd.enable = true;
       environment.etc = {
         "machine-id".text = builtins.hashString "md5" (config.networking.hostName) + "\n";
         "NIXOS".text = "";
