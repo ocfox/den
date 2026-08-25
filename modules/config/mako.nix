a15926afba30583ede42cc2cf5e813fed38ae59d
sarin <me@s4r.in>
2026-08-25 19:21:37 +0800

    *

--- modules/config/mako.nix
+++ modules/config/mako.nix
@@ -7,14 +7,51 @@
       ...
     }:
     let
-      makoSettings = {
-        "border-size" = 0;
-        "ignore-timeout" = 1;
-        "default-timeout" = 0;
-        "border-color" = "#83b6af00";
-        "background-color" = "#2b3339CC";
-        font = "Sarasa Gothic J 12";
-      };
+      makoConfig = ''
+        max-visible=5
+        sort=-time
+        layer=overlay
+        anchor=top-right
+        width=360
+        height=130
+        margin=16
+        padding=12,16
+        border-size=2
+        border-radius=4
+        font=Sarasa Gothic J 11
+        icons=1
+        max-icon-size=48
+        background-color=#2b3339F5
+        text-color=#d3c6aa
+        border-color=#83b6af
+        progress-color=over #a7c080
+        default-timeout=5000
+        ignore-timeout=0
+        on-button-left=dismiss
+        on-button-right=dismiss-all
+        on-button-middle=invoke-default-action
+
+        [urgency=low]
+        background-color=#2b3339F5
+        border-color=#7a8478
+        text-color=#9da9a0
+        default-timeout=3000
+
+        [urgency=normal]
+        background-color=#2b3339F5
+        border-color=#83b6af
+        text-color=#d3c6aa
+        default-timeout=5000
+
+        [urgency=critical]
+        background-color=#2b3339F5
+        border-color=#e68183
+        text-color=#e68183
+        default-timeout=0
+
+        [app-name=grimshot]
+        default-timeout=2500
+      '';
     in
     {
       my.packages = [ pkgs.mako ];
@@ -32,6 +69,6 @@
         };
       };
 
-      my.config."mako/config" = pkgs.writeText "mako-config" (lib.generators.toKeyValue { } makoSettings);
+      my.config."mako/config" = pkgs.writeText "mako-config" makoConfig;
     };
 }
